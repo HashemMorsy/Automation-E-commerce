0d8f36690156ef6cbdfd2278a28c7cd8fbc388ec
Hashem Morsy <113996353+HashemMorsy@users.noreply.github.com>
2022-11-23 16:48:24 +0200

    Update runners.java

--- src/test/java/org/example/testRunner/runners.java
+++ src/test/java/org/example/testRunner/runners.java
@@ -1,16 +1,17 @@
 package org.example.testRunner;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions
-        (
+@CucumberOptions(
         features = "src/main/resources/features",
-        glue = {"stepDefs"},
+        glue = {"org.example.stepDefs"},
         plugin = { "pretty",
                 "html:target/cucumber.html",
                 "json:target/cucumber.json",
                 "junit:target/cukes.xml",
                 "rerun:target/rerun.txt"},
         tags = "@smoke"
-        )
-public class runners {
+)
+public class TestRunner extends AbstractTestNGCucumberTests {
 }
